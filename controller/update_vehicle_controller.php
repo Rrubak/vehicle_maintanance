421900ff205ab4d1e9552601273974b99e8ae43b
Rubak kumar <14cse11@gmail.com>
2016-10-09 17:54:25 +0530

    updated db

--- controller/update_vehicle_controller.php
+++ controller/update_vehicle_controller.php
@@ -4,9 +4,9 @@
 	$condition = "`id` = '".$_POST['id']."'";
 	$values = update($_POST, '`bus_details`', $condition, $conn);
 	if($values == "updated"){
-		header("Location:../view/add_vehicle.php?status=inserted");
+		header("Location:../view/update_vehicles.php?status=inserted");
 	}else{
-		header("Location:../view/add_vehicle.php?status=notinserted");
+		header("Location:../view/update_vehicles.php?status=notinserted");
 	}
 ?>
 <?php include_once '../view/footer.php'; ?>
